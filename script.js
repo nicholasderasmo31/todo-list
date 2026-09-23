1f67ddbabd54019ac3942b8668cc82b486dd4571
nicholasderasmo31 <nicholasderasmo21@gmail.com>
2026-09-23 19:52:55 -0400

    finished hw3

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 // 1. Start with an array of strings (ex: "grapes", "bread", "tea")
 let todoItems = [
-    // add more items here
+    "bread",
+    "Soup",
+    "Mutton"
 ];
 
 
@@ -11,13 +13,27 @@ let todoItems = [
 const addItemButton = document.getElementById('add-item-button');
 // add more variables below
 const list = document.getElementById('list');
+const itemInput = document.getElementById('text');
+const sortBtn = document.getElementById('sort');
+const clearBtn = document.getElementById('clear');
+
+let popupIndex = 0;
+
 
 
 
 // 3. Write a function to display all items in the #list element
 function updateList() {
     // add your code here
+    list.innerHTML = '';
+
+for (let i = 0; i < todoItems.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = todoItems[i];
+    list.appendChild(li);
+   }
 }
+
 
 updateList();
 
@@ -26,6 +42,25 @@ updateList();
 // 4. Handle adding a new item when the form is submitted
 addItemButton.addEventListener('click', function () {
     // add your code here
+
+const newItem = itemInput.value.trim();
+
+if (newItem !=='') {
+    todoItems.push(newItem);
+    updateList();
+    itemInput.value = '';
+
+}
+const imageId = 'img-' + newItem.toLowerCase();
+const matchingImage = document.getElementById(imageId);
+
+if (matchingImage) {
+    matchingImage.style.display = 'block';
+    setTimeout(function () {
+        matchingImage.style.display = 'none';
+
+    }, 1600);
+}
 });
 
 
@@ -34,6 +69,8 @@ addItemButton.addEventListener('click', function () {
 // 5. Sort items alphabetically when sortBtn is clicked
 sortBtn.addEventListener("click", () => {
     // add your code here
+    todoItems.sort();
+    updateList();
 });
 
 
@@ -42,6 +79,13 @@ sortBtn.addEventListener("click", () => {
 // 6. Clear all items when clearBtn is clicked
 clearBtn.addEventListener("click", () => {
     // add your code here
+    todoItems = [];
+    updateList();
 });
+
+
+
+
+
 
 
